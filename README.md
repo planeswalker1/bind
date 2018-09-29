2b0bec7b1d82610d4cc1d960a5778022de35dd78
Daniel Munoz <daniel.munozdev@gmail.com>
2018-09-28 21:46:46 -0700

    add start files

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # bind
-
 A website about Vue data binding
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>What is Vue Data Binding</h1>
+      <p>
+        It keeps your data (i.e. your Javascript variables, arrays etc) in sync with your DOM without you having to do anything.
+        If we want to bind data to an attribute we can use v-bind: and use {{ property }} for everything else.
+        If we want to dynamically bind data we can use v-model:.
+        This website showcases some practical uses of Vue data binding.
+      </p>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: sans-serif;
+    margin: 0;
+  }
+</style>
