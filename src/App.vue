89ae7db2ef3968abc2e38c07d0f6bef0917c67ea
Daniel Munoz <daniel.munozdev@gmail.com>
2018-09-30 19:28:26 -0700

    add component

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,26 @@
       <p>
         It keeps your data (i.e. your Javascript variables, arrays etc) in sync with your DOM without you having to do anything.
         If we want to bind data to an attribute we can use v-bind: and use {{ property }} for everything else.
-        If we want to dynamically bind data we can use v-model:.
+        If we want to dynamically change data we can use v-model:.
         This website showcases some practical uses of Vue data binding.
       </p>
     </header>
+    <addBlog></addBlog>
   </div>
 </template>
 
 <script>
-export default {
+import newBlog from './components/new-blog.vue'
 
+export default {
+  components: {
+    addBlog: newBlog
+  },
+  data() {
+    return {
+      property: '{{ property }}'
+    };
+  }
 }
 </script>
 
@@ -31,6 +41,12 @@ export default {
 
   body {
     font-family: sans-serif;
+    line-height: 1.5;
     margin: 0;
+  }
+
+  .container {
+    margin: 0 auto;
+    max-width: 1360px;
   }
 </style>
